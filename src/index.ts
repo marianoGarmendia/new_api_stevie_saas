c52c45967175a6a31f9aaf81370064807d8c7ba8
unknown <marianogarmendia77@gmail.com>
2025-01-18 21:14:02 -0300

    cors fixed

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import { fileRouter } from "./Routes/files.route";
 const app = express();
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173" ,
     credentials: true,
   })
 );
