ce1d9ee0082d5a4a333a287d178cdfe3b9197c16
Mariano Garmendia <marianogarmendia77@gmail.com>
2025-01-18 23:33:08 -0300

    PORT ADDED

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ app.get("/", (req, res) => {
 //   console.log(req.file);
 //   res.send("terminó");
 // });
-
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("Server is running on port 5000");
 });
