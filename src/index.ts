cd4d107d8e97596984fa14176c2aafbf37862679
Mariano Garmendia <marianogarmendia77@gmail.com>
2025-01-18 23:41:27 -0300

    PORT FIXED

--- src/index.ts
+++ src/index.ts
@@ -47,5 +47,5 @@ app.get("/", (req, res) => {
 // });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Server is running on port 5000");
+  console.log("Server is running on port: " + PORT);
 });
